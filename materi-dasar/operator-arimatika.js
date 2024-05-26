20f6b6f7bf47fc48f3c6f62c2ea1a260978588cf
Fajar Saputra <fajarsaputra9513@gmail.com>
2024-05-26 18:47:55 +0700

    edit the file changing the .html format to .js

---
+++ materi-dasar/operator-arimatika.js
@@ -0,0 +1,8 @@
+alert(
+    "Operator Aritmatika adalah operator yang digunakan untuk melakukan operasi aritmatika, yaitu operasi-operasi aritmatika dasar seperti penjumlahan (+), pengurangan (-), perkalian (*), dan pembagian (/)."
+);
+alert("contoh penjumlahan : 10 + 5 = " + (10 + 5));
+alert("contoh pengurangan : 10 - 5 = " + (10 - 5));
+alert("contoh perkalian : 10 * 5 = " + 10 * 5);
+alert("contoh pembagian : 10/ 5 = " + 10 / 5);
+alert("contoh modulus : 10 % 5 = " + (10 % 5));
