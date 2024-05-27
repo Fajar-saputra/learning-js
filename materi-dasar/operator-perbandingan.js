7745fe5ec07dfe037a949c2b6e39543c68a907a0
Fajar Saputra <fajarsaputra9513@gmail.com>
2024-05-27 23:05:56 +0700

    add lessons about variables and rules for writing variables in javascript

--- materi-dasar/operator-perbandingan.js
+++ materi-dasar/operator-perbandingan.js
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Operator Perbandingan</title>
-    </head>
-    <body>
-        <script>
-            alert("Operator Perbandingan adalah operator yang digunakan untuk melakukan perbandingan yakni ==, !=, >, <, >=, <= pada sebuah kondisi.");
-            alert("contoh jika a = 10 dan b = 10 maka a == b = " + (10 == 10));
-            alert("contoh jika a = 10 dan b = 10 maka a != b = " + (10 != 10));
-            alert("contoh jika a = 10 dan b = 10 maka  a > b = " + (10 > 10));
-            alert("contoh jika a = 10 dan b = 10 maka  a < b = " + (10 < 10));
-            alert("contoh jika a = 10 dan b = 10 maka a >= b = " + (10 >= 10));
-            alert("contoh jika a = 10 dan b = 10 maka a <= b = " + (10 <= 10));
-        </script>
-    </body>
-</html>
+alert("Operator Perbandingan adalah operator yang digunakan untuk melakukan perbandingan yakni ==, !=, >, <, >=, <= pada sebuah kondisi.");
+alert("contoh jika a = 10 dan b = 10 maka a == b = " + (10 == 10));
+alert("contoh jika a = 10 dan b = 10 maka a != b = " + (10 != 10));
+alert("contoh jika a = 10 dan b = 10 maka  a > b = " + (10 > 10));
+alert("contoh jika a = 10 dan b = 10 maka  a < b = " + (10 < 10));
+alert("contoh jika a = 10 dan b = 10 maka a >= b = " + (10 >= 10));
+alert("contoh jika a = 10 dan b = 10 maka a <= b = " + (10 <= 10));
