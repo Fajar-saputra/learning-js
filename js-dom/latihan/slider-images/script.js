324fffbddd5faf1ef1383fadf146c5048435f478
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-03-11 23:14:23 +0700

    feat(js-dom/latihan/slider-images): add section carousel css and js

---
+++ js-dom/latihan/slider-images/script.js
@@ -0,0 +1,17 @@
+// get element html
+const carouselSlide = document.querySelector(".carousel-slide"),
+    carouselImages = document.querySelectorAll(".carousel-slide img"),
+    prevBtn = document.querySelector(".btn-prev"),
+    nextBtn = document.querySelector(".btn-next"),
+    indicator = document.querySelectorAll(".indicator"),
+    totalImages = carouselImages.length;
+
+// state current
+let currentIndex = 0,
+    // variable to aotu slide
+    autoSlideInterval;
+
+
+function updateCarousel() {
+        cla
+    }
