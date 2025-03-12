6e6e00def8780e9dbfbd76750b30272ae500c7cc
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-03-12 10:57:55 +0700

    feat( js-dom/latihan/slider-images): complete the project

--- js-dom/latihan/slider-images/script.js
+++ js-dom/latihan/slider-images/script.js
@@ -1,9 +1,9 @@
 // get element html
-const carouselSlide = document.querySelector(".carousel-slide"),
-    carouselImages = document.querySelectorAll(".carousel-slide img"),
+const carouselSlide = document.querySelector(".carousel__slide"),
+    carouselImages = document.querySelectorAll(".carousel__slide img"),
     prevBtn = document.querySelector(".btn-prev"),
     nextBtn = document.querySelector(".btn-next"),
-    indicator = document.querySelectorAll(".indicator"),
+    indicators = document.querySelectorAll(".indicator"),
     totalImages = carouselImages.length;
 
 // state current
@@ -11,7 +11,34 @@ let currentIndex = 0,
     // variable to aotu slide
     autoSlideInterval;
 
-
 function updateCarousel() {
-        cla
-    }
+    carouselSlide.style.transform = `translateX(${-currentIndex * 100}%)`;
+
+
+    // update active indicators
+    indicators.forEach((indicator, index) => {
+        indicator.classList.toggle("active", index === currentIndex);
+    });
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalImages;
+    updateCarousel()
+    resetAutoSlide()
+
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+    updateCarousel()
+    resetAutoSlide()
+}
+
+function resetAutoSlide() {
+    clearInterval(autoSlideInterval)
+
+    autoSlideInterval = setInterval(nextSlide, 5000)
+}
+
+nextBtn.addEventListener('click', nextSlide)
+prevBtn.addEventListener('click', prevSlide)
