7728c64d6b70cdf4c500b8df58f450c347049182
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-03-26 23:35:34 +0700

    feat(projects-javascript/15-to-do-list): add new projcet

--- projects-javascript/15-to-do-list/script.js
+++ projects-javascript/15-to-do-list/script.js
@@ -1,26 +1,52 @@
-let buttonAdd = document.querySelector(".todo__button");
-let inputUser = document.querySelector(".todo__input");
+let buttonAdd = document.querySelector(".todo__button"),
+    inputUser = document.querySelector(".todo__input"),
+    hapusBtn = document.querySelector(".hapus__list"),
+    // coretBtn = document.querySelector(".coret__list"),
+    li = document.querySelector(".list__item-todo");
 
-// function crateList() {
-    let listItem = document.querySelector(".list__item");
-    let liElement = document.createElement("li").classList.add("list__item-todo");
-    let paragraf = document.createElement("p");
-    let buttonsList = document.createElement("div").classList.add("buttons__list");
-    let buttonCoret = document.createElement("span").classList.add("coret__list");
-    let buttonHapus = document.createElement("span").classList.add("hapus__list");
+let listItem = document.querySelector(".list__item");
+let liElement = document.createElement("li");
+let paragraf = document.createElement("p");
+let buttonsList = document.createElement("div");
+let buttonCoret = document.createElement("span");
+let buttonHapus = document.createElement("span");
 
-console.log(listItem)
+liElement.classList.add("list__item-todo");
+buttonsList.classList.add("buttons__list");
+buttonCoret.classList.add("coret__list");
+buttonHapus.classList.add("hapus__list");
+
+function crateList() {
+    console.log(listItem);
+
+    if (!inputUser.value) {
+        alert("Tambah kegiatan dulu!!");
+        return;
+    }
 
     paragraf.textContent = inputUser.value;
 
-    buttonsList.appendChild(buttonHapus);
     buttonsList.appendChild(buttonCoret);
+    buttonsList.appendChild(buttonHapus);
 
     liElement.appendChild(paragraf);
     liElement.appendChild(buttonsList);
 
     listItem.appendChild(liElement);
-// }
+}
 
+let coretBtn = document.querySelectorAll(".coret__list");
 
-// buttonAdd.addEventListener("click", crateList);
+coretBtn.forEach((element) => {
+    element.addEventListener("click", () => {
+        li.classList.toggle("coret");
+    });
+});
+
+// coretBtn.addEventListener('click', () => {
+//     li.classList.toggle('coret');
+// })
+
+buttonAdd.addEventListener("click", () => {
+    crateList();
+});
---
+++ projects-javascript/15-to-do-list/test-aja.js
@@ -0,0 +1,20 @@
+const elementLists = document.querySelectorAll(".test__aja li");
+
+const buttonDeletes = document.querySelector(".hapus__list");
+
+
+// elementLists.forEach(element, index => {
+//     console.log(`list item : ${element}, index : ${index}`)
+// });
+
+
+let angka = [1,3,4,4,5,6]
+
+console.log(angka.Max_value)
+
+// <!-- <li class="list__item-todo">
+//                     <p>isi list to do</p>
+//                     <div class="buttons__list">
+//                         <span class="coret__list"></span> <span class="hapus__list"></span>
+//                     </div>
+//                 </li> -->
