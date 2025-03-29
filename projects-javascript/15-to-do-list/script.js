97d5718c08eeff43beff6af0a17709fad0d31d7d
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-03-29 14:03:20 +0700

    feat(projects-javascript/15-to-do-list): try again about to do list

--- projects-javascript/15-to-do-list/script.js
+++ projects-javascript/15-to-do-list/script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // fungsi menambahkan tugas
     function addTask() {
-        if (input.value.trim() === "") {
-            alert("Add Task please!!");
+        if (!input.value.trim()) {
+            alert("add tasks");
             return;
         }
 
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // bungkus
         const buttonsList = document.createElement("div");
-        buttonsList.classList.add('buttons__list')
+        buttonsList.classList.add("buttons__list");
 
         //  button hapus
         const buttonHapus = document.createElement("span");
@@ -27,8 +27,8 @@ document.addEventListener("DOMContentLoaded", () => {
         buttonCoret.classList.add("coret__list");
 
         // button edit
-        const buttonEdit = document.createElement('span')
-        buttonEdit.classList.add('edit__list')
+        const buttonEdit = document.createElement("span");
+        buttonEdit.classList.add("edit__list");
 
         // paragraf
         let paragraf = document.createElement("p");
@@ -38,29 +38,26 @@ document.addEventListener("DOMContentLoaded", () => {
         buttonsList.appendChild(buttonCoret);
         buttonsList.appendChild(buttonHapus);
         buttonsList.appendChild(buttonEdit);
-        li.appendChild(buttonsList)
+        li.appendChild(buttonsList);
 
         todoList.appendChild(li);
 
-        //  tambahkan even listener untuk hapus tugas
-        buttonHapus.addEventListener("click", () => {
-            li.remove();
-        });
-
-        // tambahkan event listener untuk coret tugas
         buttonCoret.addEventListener("click", () => {
             li.style.textDecoration = "line-through";
         });
 
-        // tambahkan event listener untuk edit tugas
-        buttonEdit.addEventListener('click', () => {
-            let newText = prompt('Edit Tugas: ', paragraf.textContent);
-            if (paragraf !== null && paragraf.trim() !== '') {
+        buttonEdit.addEventListener("click", () => {
+            let newText = prompt("Edit Task: ", paragraf.textContent);
+            if (paragraf !== null && newText.trim() !== "") {
                 paragraf.textContent = newText;
+                console.log(newText);
             }
+        });
+
+        buttonHapus.addEventListener("click", () => {
+            li.remove()
         })
 
-        // kosongkan input setelah menambahkan tuas
         input.value = "";
     }
 
@@ -68,10 +65,86 @@ document.addEventListener("DOMContentLoaded", () => {
     addButton.addEventListener("click", addTask);
 
     // event listener untuk enter key
-    input.addEventListener("keydown", (event) => {
-        if (event.key === "enter") {
+    input.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
             addTask();
-            alert("test");
         }
     });
 });
+// document.addEventListener("DOMContentLoaded", () => {
+//     const input = document.querySelector(".todo__input"),
+//         addButton = document.querySelector(".todo__button"),
+//         todoList = document.querySelector(".todo__list");
+
+//     // fungsi menambahkan tugas
+//     function addTask() {
+//         if (input.value.trim() === "") {
+//             alert("Add Task please!!");
+//             return;
+//         }
+
+//         // buat elemen list li
+//         const li = document.createElement("li");
+//         li.classList.add("list__item-todo");
+
+//         // bungkus
+//         const buttonsList = document.createElement("div");
+//         buttonsList.classList.add('buttons__list')
+
+//         //  button hapus
+//         const buttonHapus = document.createElement("span");
+//         buttonHapus.classList.add("hapus__list");
+
+//         //  button coret
+//         const buttonCoret = document.createElement("span");
+//         buttonCoret.classList.add("coret__list");
+
+//         // button edit
+//         const buttonEdit = document.createElement('span')
+//         buttonEdit.classList.add('edit__list')
+
+//         // paragraf
+//         let paragraf = document.createElement("p");
+//         paragraf.textContent = input.value;
+
+//         li.appendChild(paragraf);
+//         buttonsList.appendChild(buttonCoret);
+//         buttonsList.appendChild(buttonHapus);
+//         buttonsList.appendChild(buttonEdit);
+//         li.appendChild(buttonsList)
+
+//         todoList.appendChild(li);
+
+//         //  tambahkan even listener untuk hapus tugas
+//         buttonHapus.addEventListener("click", () => {
+//             li.remove();
+//         });
+
+//         // tambahkan event listener untuk coret tugas
+//         buttonCoret.addEventListener("click", () => {
+//             li.style.textDecoration = "line-through";
+//         });
+
+//         // tambahkan event listener untuk edit tugas
+//         buttonEdit.addEventListener('click', () => {
+//             let newText = prompt('Edit Tugas: ', paragraf.textContent);
+//             if (paragraf !== null && paragraf.trim() !== '') {
+//                 paragraf.textContent = newText;
+//             }
+//         })
+
+//         // kosongkan input setelah menambahkan tuas
+//         input.value = "";
+//     }
+
+//     // Event listner tombol tambah
+//     addButton.addEventListener("click", addTask);
+
+//     // event listener untuk enter key
+//     input.addEventListener("keydown", (event) => {
+//         if (event.key === "enter") {
+//             addTask();
+//             alert("test");
+//         }
+//     });
+// });
