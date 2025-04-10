94ff78a91e2ed211a35e3c7ecfd7264166ea5b3c
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-04-10 19:07:08 +0700

    feat(projects-javascript/16-number-guessing-game): add new project for exercise javascript

---
+++ projects-javascript/16-number-guessing-game/script.js
@@ -0,0 +1,47 @@
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+// let randomNumber =2;
+
+let attemps = 0;
+
+// get element
+const starBtn = document.querySelector(".star");
+const userInput = document.querySelector("#userInput");
+const message = document.querySelector(".message");
+
+
+
+function gameplay() {
+    let guess = parseInt(userInput.value);
+
+    attemps++;
+    
+
+    if (guess === randomNumber) {
+        alert(`Selamat Tebakan Kamu benar ${randomNumber} dalam ${attemps} percobaan`);
+
+        message.textContent = message.textContent;
+        message.style.color = "green";
+
+        resetAttemps()
+    } else if (guess > randomNumber) {
+        console.log(`Tebakan Kamu Salah!`);
+
+        message.textContent = message.textContent;
+        message.style.color = "red";
+    }  else if (guess < randomNumber) {
+        console.log(`Tebakan Kamu Salah!`);
+
+        message.textContent = message.textContent;
+        message.style.color = "red";
+    }
+
+    userInput.value = ""
+}
+
+function resetAttemps() {
+    let attemps = 1;
+    let randomNumber = Math.floor(Math.random() * 10) + 1;
+}
+
+starBtn.addEventListener('click', gameplay)
+
