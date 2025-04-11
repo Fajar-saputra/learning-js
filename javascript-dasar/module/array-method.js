f07d0fb035497a309e0b79bddea36bf60f154eb9
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-04-11 23:07:29 +0700

    reviewing about material js

---
+++ javascript-dasar/module/array-method.js
@@ -0,0 +1,27 @@
+// Array Methods
+// concat()
+// Join several arrays into one
+
+// let pertama = [1,2,3,4,5]
+// let kedua = [6,7,8,9,10]
+// let baru = pertama.concat(kedua)
+
+// console.log(baru);
+
+// indexOf()
+// Returns the first position at which a given element appears in an array
+
+let kalimat = 'hello world'
+let kedua = [6,7,8,9,10]
+
+console.log(kalimat.indexOf('h'));
+console.log(kalimat.indexOf('e'));
+console.log(kalimat.indexOf('l'));
+console.log(kalimat.indexOf('w'));
+console.log(kalimat.indexOf('o'));
+console.log(kalimat.indexOf('r'));
+console.log(kalimat.indexOf('d'));
+console.log(kalimat.indexOf('hello'));
+console.log(kalimat.indexOf('world'));
+
+
