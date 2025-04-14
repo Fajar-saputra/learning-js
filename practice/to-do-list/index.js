e6e0ed9591f2b4dac17cb507fb41ce1a7ea1f6d2
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-04-14 22:45:32 +0700

    feat(practice/to-do-list): reviewing about material js

---
+++ practice/to-do-list/index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    function createTasks() {
+        const ul = document.querySelector('.tasks')
+        const input = document.querySelector('input')
+
+        const li = document.createElement('li')
+
+        li.textContent = input.value;
+
+        ul.appendChild(li)
+    }
+
+    function tamil() {
+        const listTasks = tasks;
+
+        tasks.array.forEach(element => {
+            
+        });
+    }
+
+    function tasks(tasks) {
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+})
